aa7bb96da06adbdc4d3701db45d6cc4b2d071ffb
xingzhiyuan000 <11353401+xingzhiyuan000@user.noreply.gitee.com>
2022-09-12 22:43:38 +0800

    更新

--- nets/Wang_DS_ViT_RGB.py
+++ nets/Wang_DS_ViT_RGB.py
@@ -28,7 +28,7 @@ def drop_path(x, drop_prob: float = 0., training: bool = False):
 
 class DropPath(nn.Module):
     """
-    Drop paths (Stochastic Depth) per sample  (when applied in main path of residual blocks).
+    Drop paths (Stochastic Depth) per sample  (when applied in main path of residual blocks).123
     """
     def __init__(self, drop_prob=None):
         super(DropPath, self).__init__()
